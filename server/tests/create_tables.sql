e60b8afcbaac3d9320323e072eb538363594a58e
Andreas Tolnes <andrtoln@stud.ntnu.no>
2019-11-20 22:42:58 +0100

    Created own test folder

---
+++ server/tests/create_tables.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS article;
+
+CREATE TABLE article (
+    article_id INT NOT NULL UNSIGNED AUTO_INCREMENT,
+    category_id INT NOT NULL,
+    title VARCHAR(100),
+    summary VARCHAR(150),
+    article_text MEDIUMTEXT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    image VARCHAR(200),
+    category VARCHAR(20),
+    priority TINYINT,
+
+    PRIMARY KEY (article_id),
+);
