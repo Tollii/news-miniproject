a8561a2cdbdfa85aba98a04ce67e0591a821f6a8
Andreas Tolnes <andrtoln@stud.ntnu.no>
2019-11-23 16:10:56 +0100

    Trying to fix the test

--- server/dao/articledao.test.js
+++ server/dao/articledao.test.js
@@ -26,22 +26,24 @@ afterAll( () => {
    pool.end();
 });
 
-// test('get one article from db', done => {
-//    function callback(status, data) {
-//        console.log('Test callback: status=' + status + ', data=' + JSON.stringify(data));
-//        expect(data.length).toBe(1);
-//        expect(data[0].title).toBe('Guards Protect Fitchburg Plants From Sabotage');
-//        done();
-//    }
-//    articleDao.getOne(0, callback);
-// });
+/*
+test('get one article from db', done => {
+   function callback(status, data) {
+       console.log('Test callback: status=' + status + ', data=' + JSON.stringify(data));
+       expect(data.length).toBe(1);
+       expect(data[0].title).toBe('Guards Protect Fitchburg Plants From Sabotage');
+       done();
+   }
+   articleDao.getOne(0, callback);
+});
+*/
 
 test('get all articles from db', done => {
     function callback(status, data) {
         console.log("Test: get all articles from db");
-        console.log('Test callback: status=' + status + ', data=' + JSON.stringify(data));
+        console.log('Test callback: status=' + status + ', data=' + data.length);
         expect(data.length).toBeGreaterThanOrEqual(2);
         done();
     };
-    articleDao.getAll(callback());
+    articleDao.getAll(callback);
 });
