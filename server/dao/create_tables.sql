78098da909d50b19f66024ca4b12dfaaac2b060a
Andreas Tolnes <andrtoln@stud.ntnu.no>
2019-11-23 16:27:14 +0100

    yeet

--- server/dao/create_tables.sql
+++ server/dao/create_tables.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS article;
 
 CREATE TABLE article (
-    article_id INT NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    article_id INT AUTO_INCREMENT PRIMARY KEY,
     category_id INT NOT NULL,
     title VARCHAR(100),
     summary VARCHAR(150),
@@ -9,5 +9,5 @@ CREATE TABLE article (
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     image VARCHAR(200),
     category VARCHAR(20),
-    priority TINYINT,
-)
+    priority TINYINT
+);
